a581e91ddbd27499635cf766ba9a2c6aa662514b
Lucas Colley <lucas.colley8@gmail.com>
2025-02-05 15:26:18 +0000

    improvements

--- src/unit_api/__init__.py
+++ src/unit_api/__init__.py
@@ -1,9 +1,9 @@
 """Unit API."""
 
-from typing import Any, Protocol, Self, runtime_checkable
+from typing import Any, Protocol, Final, Self, runtime_checkable
 
-__version__ = "0.0.1.dev0"
-__all__ = ["Unit"]
+__version__: Final = "0.0.1.dev0"
+__all__ = ["__version__", "Unit"]
 
 type Dimension = Any  # TODO: dimension-api
 type Quantity = Any  # TODO: quantity-api
@@ -13,9 +13,8 @@ class Unit(Protocol):
     @property
     def dimension(self) -> Dimension: ...
 
-    def __eq__(self, other: Self, /) -> bool: ...
     def __mul__(self, other: Self, /) -> Self: ...
-    def __div__(self, other: Self, /) -> Self: ...
+    def __truediv__(self, other: Self, /) -> Self: ...
     def __pow__(self, other: int | Self, /) -> Self: ...
 
     # debatable whether this should be standardised
